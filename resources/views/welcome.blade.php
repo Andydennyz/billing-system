294c1721aa18778410c621d373c5c05bb9c5703d
Andydennyz <andywesonga@gmail.com>
2026-02-01 15:39:18 -0800

    Laravel set up complete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,5 +7,6 @@
 <body style="background:black;color:white">
     <h1>🔥  THIS IS MY EDITED PAGE 🔥</h1>
     <h1>Hello Laravel i am dennis a developer</h1>
+    <p>Laravel set up is now complete!</p>
 </body>
 </html>
